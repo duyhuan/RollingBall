d36d83f8f6a13bf8aa9b101020e7a56c63c5fc2e
techmaster <techmaster@techmasters-iMac-3.local>
2017-01-17 09:27:37 +0700

    RollingBall

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+Writterd by Ngo Duy Huan
--- RollingBall/ViewController.swift
+++ RollingBall/ViewController.swift
@@ -10,16 +10,49 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var ball = UIImageView()
+    let ballRadius: CGFloat = 32.0
+    var angle = 1 * CGFloat(M_PI/180)
+    var timer1 = Timer()
+    var timer2 = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        addBall()
+        startTimer1()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func startTimer1() {
+        timer1 = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(ViewController.rollBallLeftToRight), userInfo: nil, repeats: true)
     }
-
-
+    
+    func stopTimer1() {
+        timer1.invalidate()
+    }
+    
+    func addBall() {
+        ball = UIImageView(image: UIImage(named: "ball.png"))
+        ball.frame.size = CGSize(width: ballRadius*2, height: ballRadius*2)
+        ball.center = CGPoint(x: ballRadius, y: self.view.bounds.height/2)
+        view.addSubview(ball)
+    }
+    
+    func rollBallLeftToRight() {
+        ball.transform = self.ball.transform.rotated(by: angle)
+        ball.center = CGPoint(x: ball.center.x + ballRadius * angle, y: ball.center.y)
+        if ball.center.x >= view.bounds.size.width - ballRadius {
+            stopTimer1()
+            timer2 = Timer.scheduledTimer(timeInterval: 0.001, target: self, selector: #selector(ViewController.rollBallRightToLeft), userInfo: nil, repeats: true)
+        }
+    }
+    
+    func rollBallRightToLeft() {
+        ball.transform = self.ball.transform.rotated(by: -angle)
+        ball.center = CGPoint(x: ball.center.x - ballRadius * angle, y: ball.center.y)
+        if ball.center.x <= ballRadius {
+            timer2.invalidate()
+            startTimer1()
+        }
+    }
 }
 
